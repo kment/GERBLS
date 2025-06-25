9325b9ce2b5dc8975128ffe4e54e3f1c5a6b8933
Kristo Ment <kristo.ment@gmail.com>
2025-06-25 14:38:44 -0400

    Run clang-format to improve readability and consistency

--- src/gerbls/cpp/ffafunc.cpp
+++ src/gerbls/cpp/ffafunc.cpp
@@ -20,20 +20,25 @@
 
 // out = x - y
 template <typename T>
-void array_diff(const T* __restrict__ x, const T* __restrict__ y, const size_t size, T* __restrict__ out) {
-    
-    for (size_t i = 0; i < size; i++)
-        out[i] = x[i] - y[i];
-    
+void array_diff(const T *__restrict__ x,
+                const T *__restrict__ y,
+                const size_t size,
+                T *__restrict__ out)
+{
+    for (size_t i = 0; i < size; i++) out[i] = x[i] - y[i];
 }
 
 // Find maximum dchi2 given sum arrays of in-transit (mag*wts) and (wts)
 // Returns: BLSResult corresponding to dchi2_max
 // wtotal = sum(wts)
 template <typename T>
-void array_dchi2_max(const T* __restrict__ prod, const T* __restrict__ wts, const size_t size, const T wtotal, 
-                     BLSResult<T>& result, const size_t width) {
-    
+void array_dchi2_max(const T *__restrict__ prod,
+                     const T *__restrict__ wts,
+                     const size_t size,
+                     const T wtotal,
+                     BLSResult<T> &result,
+                     const size_t width)
+{
     size_t i_max = 0;
     T dchi2, dchi2_max = 0;
 
@@ -56,21 +61,30 @@ void array_dchi2_max(const T* __restrict__ prod, const T* __restrict__ wts, cons
 
 // Output: results (must be array of size wprod.rows)
 template <typename T>
-void chisq_2d(riptide::ConstBlock<T> wprod, riptide::ConstBlock<T> weights, const size_t min_width, const size_t max_width, BLSResult<T>* results) {
-
-    for (size_t i = 0; i < wprod.rows; ++i)
-        {
-        //snr1(block.rowptr(i), block.cols, widths, stdnoise, out);
-        chisq_row<T>(wprod.rowptr(i), weights.rowptr(i), wprod.cols, min_width, max_width, *results);
+void chisq_2d(riptide::ConstBlock<T> wprod,
+              riptide::ConstBlock<T> weights,
+              const size_t min_width,
+              const size_t max_width,
+              BLSResult<T> *results)
+{
+    for (size_t i = 0; i < wprod.rows; ++i) {
+        // snr1(block.rowptr(i), block.cols, widths, stdnoise, out);
+        chisq_row<T>(
+            wprod.rowptr(i), weights.rowptr(i), wprod.cols, min_width, max_width, *results);
         results++;
-        }
+    }
 }
 
 // Evaluate dchi2 for a fixed period (FFA row)
 // Output: BLSResult corresponding to the highest dchi2
 template <typename T>
-void chisq_row(const T* __restrict__ wprod, const T* __restrict__ wts, const size_t size, const size_t min_width, const size_t max_width, BLSResult<T>& result) {
-
+void chisq_row(const T *__restrict__ wprod,
+               const T *__restrict__ wts,
+               const size_t size,
+               const size_t min_width,
+               const size_t max_width,
+               BLSResult<T> &result)
+{
     T cpfsum1[size + max_width];
     T cpfsum2[size + max_width];
     T inmag[size];
@@ -80,29 +94,27 @@ void chisq_row(const T* __restrict__ wprod, const T* __restrict__ wts, const siz
     const T wtotal = cpfsum2[size - 1]; // sum of weights
 
     for (size_t width = min_width; width <= max_width; width++) {
-
         array_diff<T>(cpfsum1 + width, cpfsum1, size, inmag);
         array_diff<T>(cpfsum2 + width, cpfsum2, size, inwts);
         array_dchi2_max<T>(inmag, inwts, size, wtotal, result, width);
-        
     }
 }
 
 // Compute the periodogram of a time series that has been normalised to zero mean and unit variance.
-// get_duration_limits(P) should return the min and max transit duration at the given orbital period P
-// Output: vector of BLSResult<T> containing the best fit for each tested orbital period
+// get_duration_limits(P) should return the min and max transit duration at the given orbital period
+// P Output: vector of BLSResult<T> containing the best fit for each tested orbital period
 template <typename T>
-std::vector<BLSResult<T>> periodogram(
-    const T* __restrict__ mag,
-    const T* __restrict__ wts,
-    size_t size,
-    double tsamp,
-    //const std::vector<size_t>& widths,
-    std::function<std::tuple<double, double>(double)> get_duration_limits,
-    double period_min,
-    double period_max)
-    {
-    //periodogram_check_arguments(size, tsamp, period_min, period_max, bins_min, bins_max);
+std::vector<BLSResult<T>>
+    periodogram(const T *__restrict__ mag,
+                const T *__restrict__ wts,
+                size_t size,
+                double tsamp,
+                // const std::vector<size_t>& widths,
+                std::function<std::tuple<double, double>(double)> get_duration_limits,
+                double period_min,
+                double period_max)
+{
+    // periodogram_check_arguments(size, tsamp, period_min, period_max, bins_min, bins_max);
 
     // Temporary variables
     double min_width_P, max_width_P;
@@ -110,36 +122,36 @@ std::vector<BLSResult<T>> periodogram(
     // Calculate periodogram length and allocate memory for output
     const size_t length = periodogram_length(size, tsamp, period_min, period_max);
     std::vector<BLSResult<T>> results(length);
-    BLSResult<T>* presult = results.data();
+    BLSResult<T> *presult = results.data();
 
     // Pad data with zeros to ensure the last FFA row never gets cropped
     const double period_max_samples = period_max / tsamp;
-    const size_t n = size + period_max_samples;     // Total size of the data
+    const size_t n = size + period_max_samples; // Total size of the data
 
     // Allocate buffers
     const size_t bufsize = n;
     std::unique_ptr<T[]> ffabuf_mem(new T[bufsize]);
     std::unique_ptr<T[]> ffaout_mem1(new T[bufsize]);
     std::unique_ptr<T[]> ffaout_mem2(new T[bufsize]);
-    T* ffabuf = ffabuf_mem.get();
-    T* ffamag = ffaout_mem1.get();
-    T* ffawts = ffaout_mem2.get();
+    T *ffabuf = ffabuf_mem.get();
+    T *ffamag = ffaout_mem1.get();
+    T *ffawts = ffaout_mem2.get();
 
     /* Downsampling loop */
-    //const double tau = tsamp; // current sampling time
-    //const size_t n = size; // current number of input samples
-    //const size_t num_widths = widths.size();
+    // const double tau = tsamp; // current sampling time
+    // const size_t n = size; // current number of input samples
+    // const size_t num_widths = widths.size();
 
     // Calculate weights and weighted mags
     std::unique_ptr<T[]> weights(new T[n]);
-    std::unique_ptr<T[]> wprod(new T[n]);       // Product of wmag and weights
-    double ftotal = 0;   // Weighted sum of fluxes
-    double wtotal = 0;   // Sum of weights
+    std::unique_ptr<T[]> wprod(new T[n]); // Product of wmag and weights
+    double ftotal = 0;                    // Weighted sum of fluxes
+    double wtotal = 0;                    // Sum of weights
     for (size_t i = 0; i < size; i++) {
         ftotal += mag[i] * wts[i];
         wtotal += wts[i];
     }
-    const double favg = ftotal / wtotal;  // Weighted mean flux
+    const double favg = ftotal / wtotal; // Weighted mean flux
     for (size_t i = 0; i < size; i++) {
         weights[i] = favg * favg * wts[i];
         wprod[i] = (mag[i] / favg - 1) * weights[i];
@@ -157,10 +169,9 @@ std::vector<BLSResult<T>> periodogram(
     const size_t bstop = std::min({n, size_t(period_max / tsamp)});
 
     /* FFA transform loop */
-    for (size_t bins = bstart; bins <= bstop; ++bins)
-        {
+    for (size_t bins = bstart; bins <= bstop; ++bins) {
         const size_t rows = n / bins;
-        //const float stdnoise = 1.0; //sqrt(rows * downsampled_variance(size, f));
+        // const float stdnoise = 1.0; //sqrt(rows * downsampled_variance(size, f));
         const double period_ceil = std::min(period_max_samples, bins + 1.0);
         const size_t rows_eval = std::min(rows, riptide::ceilshift(rows, bins, period_ceil));
         std::tie(min_width_P, max_width_P) = get_duration_limits((bins + 1) * tsamp);
@@ -169,74 +180,81 @@ std::vector<BLSResult<T>> periodogram(
 
         riptide::transform<T>(wprod.get(), rows, bins, ffabuf, ffamag);
         riptide::transform<T>(weights.get(), rows, bins, ffabuf, ffawts);
-        
+
         auto block1 = riptide::ConstBlock<T>(ffamag, rows_eval, bins);
         auto block2 = riptide::ConstBlock<T>(ffawts, rows_eval, bins);
         chisq_2d<T>(block1, block2, min_width, max_width, presult);
 
-        for (size_t s = 0; s < rows_eval; ++s)
-            {
+        for (size_t s = 0; s < rows_eval; ++s) {
             presult[s].P = tsamp * bins * bins / (bins - s / (rows - 1.0));
             presult[s].mag0 = favg * (presult[s].mag0 + 1);
             presult[s].dmag *= presult[s].mag0;
             presult[s].N_bins = bins;
-            }
-
-        presult += rows_eval;
         }
 
-    return results;
+        presult += rows_eval;
     }
+
+    return results;
+}
+
 // Explicit instantiations for float and double
-template std::vector<BLSResult<float>> periodogram(const float* __restrict__, const float* __restrict__, size_t, double, 
-    std::function<std::tuple<double, double>(double)>, double, double);
-template std::vector<BLSResult<double>> periodogram(const double* __restrict__, const double* __restrict__, size_t, double, 
-    std::function<std::tuple<double, double>(double)>, double, double);
+template std::vector<BLSResult<float>>
+    periodogram(const float *__restrict__,
+                const float *__restrict__,
+                size_t,
+                double,
+                std::function<std::tuple<double, double>(double)>,
+                double,
+                double);
+template std::vector<BLSResult<double>>
+    periodogram(const double *__restrict__,
+                const double *__restrict__,
+                size_t,
+                double,
+                std::function<std::tuple<double, double>(double)>,
+                double,
+                double);
 
 /*
 Returns the total number of trial periods in a periodogram
 */
-size_t periodogram_length(
-    size_t size,
-    double tsamp,
-    double period_min,
-    double period_max)
-    //size_t bins_min,
-    //size_t bins_max)
-    {
-    //periodogram_check_arguments(size, tsamp, period_min, period_max, bins_min, bins_max);
+size_t periodogram_length(size_t size, double tsamp, double period_min, double period_max)
+// size_t bins_min,
+// size_t bins_max)
+{
+    // periodogram_check_arguments(size, tsamp, period_min, period_max, bins_min, bins_max);
 
     size_t length = 0; // total number of period trials, to be calculated
 
     /* Downsampling loop */
-    //const double tau = f * tsamp; // current sampling time
+    // const double tau = f * tsamp; // current sampling time
     const double period_max_samples = period_max / tsamp;
-    //const size_t n = size; // current number of input samples
+    // const size_t n = size; // current number of input samples
 
     // Pad data with zeros to ensure the last FFA row never gets cropped
-    const size_t n = size + period_max_samples;     // Total size of the data
+    const size_t n = size + period_max_samples; // Total size of the data
 
     const size_t bstart = period_min / tsamp;
     const size_t bstop = period_max / tsamp;
 
     /* FFA transform loop */
-    for (size_t bins = bstart; bins <= bstop; ++bins)
-        {
+    for (size_t bins = bstart; bins <= bstop; ++bins) {
         const size_t rows = n / bins;
         const double period_ceil = std::min(period_max_samples, bins + 1.0);
         const size_t rows_eval = std::min(rows, riptide::ceilshift(rows, bins, period_ceil));
         length += rows_eval;
-        }
-    
-    return length;
     }
+
+    return length;
+}
 
 // Resample the light curve with a uniform sampling interval
 // Assumes the data are already time-sorted
-std::unique_ptr<DataContainer> resample_uniform(const DataContainer& data, double tsamp) {
-
-    std::unique_ptr<DataContainer> out (new DataContainer);
-    size_t N_sampled = ceil((data.rjd[data.size-1] - data.rjd[0]) / tsamp + 0.5);
+std::unique_ptr<DataContainer> resample_uniform(const DataContainer &data, double tsamp)
+{
+    std::unique_ptr<DataContainer> out(new DataContainer);
+    size_t N_sampled = ceil((data.rjd[data.size - 1] - data.rjd[0]) / tsamp + 0.5);
     out->allocate(N_sampled);
     out->valid_mask.reset(new bool[N_sampled]);
     size_t i = 0;
--- src/gerbls/cpp/model.hpp
+++ src/gerbls/cpp/model.hpp
@@ -16,89 +16,104 @@
 
 // BLS model (base class)
 struct BLSModel {
+    // Settings
+    double f_min = 0.025;             // Minimum search frequency
+    double f_max = 5;                 // Maximum search frequency
+    int duration_mode = 2;            // Affects tested transit durations
+    double min_duration_factor = 0;   // Affects get_min_duration()
+    double max_duration_factor = 0.1; // Affects get_max_duration()
 
-	// Settings
-	double f_min = 0.025;					// Minimum search frequency
-	double f_max = 5;						// Maximum search frequency
-	int duration_mode = 2;					// Affects tested transit durations
-	double min_duration_factor = 0;			// Affects get_min_duration()
-	double max_duration_factor = 0.1;		// Affects get_max_duration()
+    // Pointer to associated data
+    DataContainer *data = nullptr;
 
-	// Pointer to associated data
-	DataContainer* data = nullptr;
-	
-	// Pointer to associated target
-	const Target* target = nullptr;
+    // Pointer to associated target
+    const Target *target = nullptr;
 
-	// Array to store tested frequencies
-	std::vector<double> freq;
+    // Array to store tested frequencies
+    std::vector<double> freq;
 
-	// Constructor and destructor
-	BLSModel(DataContainer&, double=0, double=0, const Target* =nullptr, int=0, double=0, double=0);
-	virtual ~BLSModel() = default;
+    // Constructor and destructor
+    BLSModel(DataContainer &data_ref,
+             double f_min,
+             double f_max,
+             const Target *targetPtr,
+             int duration_mode,
+             double min_duration_factor,
+             double max_duration_factor);
+    virtual ~BLSModel() = default;
 
-	std::tuple<double, double> get_duration_limits(double);	// Min and max transit duration to test at a given period
-	size_t N_freq();	// Get number of frequencies
+    std::tuple<double, double>
+        get_duration_limits(double P); // Min and max transit duration to test at a given period
+    size_t N_freq();                   // Get number of frequencies
 
-	// Virtual functions to be overwritten
-	virtual void run(bool);
+    // Virtual functions to be overwritten
+    virtual void run(bool verbose);
 
-	// Required results for each tested frequency
-	std::vector<double> dchi2, chi2_mag0, chi2_dmag, chi2_t0, chi2_dt;
-
+    // Required results for each tested frequency
+    std::vector<double> dchi2, chi2_mag0, chi2_dmag, chi2_t0, chi2_dt;
 };
 
 // BLS model (brute force)
-struct BLSModel_bf: public BLSModel {
+struct BLSModel_bf : public BLSModel {
+    // Grid search ranges and steps
+    double dt_per_step = 0.003; // Maximum orbital shift between frequencies in days
+    double t_bins = 0.007;      // Time bin width in days
+    size_t N_bins_min = 100;    // Minimum number of bins
 
-	// Grid search ranges and steps
-	double dt_per_step = 0.003; 	// Maximum orbital shift between frequencies in days
-	double t_bins = 0.007; 			// Time bin width in days
-	size_t N_bins_min = 100; 			// Minimum number of bins
+    // Arrays to store best chi2 values for each tested frequency
+    std::vector<double> chi2, chi2r;
 
-	// Arrays to store best chi2 values for each tested frequency
-	std::vector<double> chi2, chi2r;
+    // Constructors
+    BLSModel_bf(DataContainer &data_ref,
+                double f_min,
+                double f_max,
+                const Target *targetPtr,
+                double dt_per_step,
+                double t_bins,
+                size_t N_bins_min,
+                int duration_mode,
+                double min_duration_factor,
+                double max_duration_factor);
+    BLSModel_bf(DataContainer &data_ref,
+                const std::vector<double> &freq,
+                const Target *targetPtr,
+                double t_bins,
+                size_t N_bins_min,
+                int duration_mode,
+                double min_duration_factor,
+                double max_duration_factor);
 
-	// Constructors
-	BLSModel_bf(DataContainer&, double=0, double=0, const Target* =nullptr,
-        	 	double=0, double=0, size_t=0, int=0, double=0, double=0);
-	BLSModel_bf(DataContainer&, const std::vector<double>&, const Target* =nullptr, 
-				double=0, size_t=0, int=0, double=0, double=0);
+    // Methods to overwrite parent virtual functions
+    void run(bool verbose = true);
 
-	// Methods to overwrite parent virtual functions
-	void run(bool=true);
-
-	// Private methods
-	private:
-		void initialize(double, size_t);
-
+    // Private methods
+private:
+    void initialize(double t_bins, size_t N_bins_min);
 };
 
 // BLS model (FFA)
-struct BLSModel_FFA: public BLSModel {
+struct BLSModel_FFA : public BLSModel {
+    // Settings
+    double t_samp = 2. / 60 / 24; // Uniform cadence to resample data to
 
-	// Settings
-	double t_samp = 2./60/24; 		// Uniform cadence to resample data to
+    // Pointer to the resampled data
+    std::unique_ptr<DataContainer> rdata;
 
-	// Pointer to the resampled data
-	std::unique_ptr<DataContainer> rdata;
+    // TEMPORARY FFA results
+    std::vector<double> periods;
+    std::vector<size_t> widths;
+    std::vector<size_t> foldbins;
+    std::vector<double> snr;
+    std::vector<size_t> t0;
 
-	// TEMPORARY FFA results
-	std::vector<double> periods;
-	std::vector<size_t> widths;
-	std::vector<size_t> foldbins;
-	std::vector<double> snr;
-	std::vector<size_t> t0;
+    // Inherit constructor from parent
+    using BLSModel::BLSModel;
 
-	// Inherit constructor from parent
-	using BLSModel::BLSModel;
-
-	// Methods
-	template <typename T> void process_results(std::vector<BLSResult<T>>&);
-	void run(bool=true);
-	void run_double(bool=true);
-	template <typename T> void run_prec(bool=true);
-
+    // Methods
+    template <typename T> void process_results(std::vector<BLSResult<T>> &results);
+    void run(bool verbose = true);
+    void run_double(bool verbose = true);
+    template <typename T> void run_prec(bool verbose = true);
 };
 
 #endif /* MODEL_HPP_ */
